7b1073cafef42047f19d6e3050aea21295410d14
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2024-01-05 20:27:57 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import { Camera, CameraType } from "expo-camera";
+import { Video } from "expo-av";
+import { shareAsync } from "expo-sharing";
+import * as MediaLibrary from "expo-media-library";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
